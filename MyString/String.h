1611b8d94fba5fd7b9edf4f8546a56ed9bab62b4
Mandi_pv66 <Mandi_pv66@29-3>
2025-09-16 14:34:21 +0300

    /

--- MyString/String.h
+++ MyString/String.h
@@ -11,8 +11,11 @@ public:
 	MyString(const MyString& obj);
 	bool MyStrStrt(const char* str);
 	int MyStrlen(); //возвращает длину строки
-	int  MyChr(char c);
+	int  MyChr(char c); // поиск символа в строке(индекс найденного символа, либо -1)
 	MyString MyStrCopy(const MyString& obj); //копирование строк
+	void MyStrCat(MyString& b); // объединение строк
+	void MyDelChr(char c); // удаляет указанный символ
+	int MyStrCmp(MyString& b); // сравнение строк
  
 };
 
